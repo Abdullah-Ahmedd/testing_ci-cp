feb96105e04b40a8c7926129146ca55d30209a6b
Abdullah-Ahmedd <163368044+Abdullah-Ahmedd@users.noreply.github.com>
2025-06-14 17:01:55 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 
-// Adds two integers.
+/**
+ * @brief Adds two integers.
+ *
+ * @param a First number
+ * @param b Second number
+ * @return Sum of a and b
+ */
 int Add(int a, int b) {
   return a + b;
 }
 
+/**
+ * @brief Main function.
+ */
 int main() {
-  std::cout << "Result: " << Add(2, 3) << std::endl;
+  int result = Add(2, 3);
+  std::cout << "Result: " << result << std::endl;
   return 0;
 }
