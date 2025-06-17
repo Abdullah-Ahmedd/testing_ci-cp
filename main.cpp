6c679d8b5a42836c3f13a5e4dd518c9df58a673c
Abdullah-Ahmedd <163368044+Abdullah-Ahmedd@users.noreply.github.com>
2025-06-17 15:24:48 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
-#include<iostream>
-using namespace std;
-int main( ){int x=0;for(int i=0;i<10;i++){x+=i;} cout<<"Sum:"<<x<<endl;return 0;}
+#include <iostream>
+
+int main() {
+    int sum = 0;
+
+    for (int i = 0; i < 10; ++i) {
+        sum += i;
+    }
+
+    std::cout << "Sum: " << sum << std::endl;
+    return 0;
+}
